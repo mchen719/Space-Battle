cd9abc07f539f25db7fdfa9ab1a44c126c9e7564
Matthew Chen <m.chen71996@mail.com>
2023-11-15 21:38:34 -0500

    Added a score tally and loop to create alien array of objects

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Ship {
      }
 
      flee() {
-        return "You gave up! But kept your life in the process! GAME OVER. Final Score: "
+        return "You gave up! But kept your life in the process! GAME OVER. Final Score: " + scoreTally(Player, enemyShips)
      }
 }
 
@@ -32,36 +32,68 @@ class EnemyShip extends Ship { //Getting negative numbers needs adressing
         super(Math.floor(Math.random() * 4) + 3, Math.floor(Math.random() * 2) + 2, (Math.floor(Math.random() * 3) + 6)/10)
     }
 }
-const bob = Math.floor(Math.random() * 4) + 3
-const USSAssembly = new PlayerShip(40, 5, .7)
-const alienVessel = new EnemyShip()
 
-const atkBtn = document.getElementById("atk__btn");
-const fleeBtn = document.getElementById("flee__btn");
+///Loop to create alien ships 
+const enemyShips = []
 
-//atkBtn.addEventListener('click', () => {return 'click'});
+for (let i = 0; i < 6; i++){
+    let newAlienShip = new EnemyShip()
+    enemyShips.push(newAlienShip)
+}
+
+console.log(enemyShips)
+
+function scoreTally(Player, arr){
+    let finalScore = 50
+    if (Player.hull < 0){
+        finalScore -= 20
+    }else{
+    let deduction = 20 - Player.hull
+    finalScore -= deduction; 
+    }
+    if (arr.length > 0) {
+        finalScore -= (arr.length * 5);
+    }
+    return finalScore;
+}
 
 function battle(Player, Enemy) {
-    while (Player.hull > 0 || Enemy.hull > 0){
+    while (Player.hull > 0 && Enemy.hull > 0){
         Player.attack(Enemy);
-        console.log(`alien has ${Enemy.hull} Your hull is ${Player.hull}`)
-        if(Enemy.hull >= 1) {
-            Enemy.attack(Player);
-        } 
+        if (enemyShips.length > 0){
+            console.log(`alien has ${Enemy.hull} Your hull is ${Player.hull}`)
+            if(Enemy.hull >= 1) {
+                Enemy.attack(Player);
+            } 
+        }
         if(Enemy.hull <= 0) {
-            console.log('Enemy ships destroyed!')
-            break;
+            console.log('Enemy ship was destroyed!')
+            enemyShips.shift();
         }
 
         if(Player.hull <= 0) {
             console.log('Your ship has been destroyed! Try again?')
-            console.log(`alien has ${Enemy.hull} Your hull is ${Player.hull}`)
+            console.log("Final Score: " + scoreTally(Player, enemyShips))
             break;
         }
+
+        if (enemyShips.length === 0){
+            console.log("You WIN")
+            console.log("Final Score: " + scoreTally(Player, enemyShips))
+        }
     }
+    console.log(enemyShips.length)
 }
+
+const USSAssembly = new PlayerShip(2, 5, .7)
+
+
+///BUTTONS AND STYLES 
+const atkBtn = document.getElementById("atk__btn");
+const fleeBtn = document.getElementById("flee__btn");
+
 atkBtn.addEventListener("click" , (e) => {
-    battle(USSAssembly, alienVessel);
+    battle(USSAssembly, enemyShips[0]);
     })
 //fleeBtn.addEventListener("click", flee());
 
@@ -72,5 +104,3 @@ atkBtn.addEventListener("click" , (e) => {
 
 // console.log(alienVessel)
 // console.log(alienVessel.hull);
-
-
